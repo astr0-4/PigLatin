25f6226fdc1f77e17807cb5adf3ae54a42e75a47
Alex Stroulger <alexstroulger@gmail.com>
2015-06-12 16:29:31 -0400

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Alex on 2015-06-12.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
